330816c6f5dcdbcdb00321ee449841b2cded5587
widyaput <wnugrah.putra@gmail.com>
2020-01-29 21:37:06 +0700

    Widya Anugrah Putra-16519307-Pembuatan fungsi-fungsi kegiatan

--- hilmy.c
+++ hilmy.c
@@ -1,2 +1,159 @@
 #include "hilmy.h"
 
+void StartStats(Stats* S){
+    Hlt(*S) = 100;
+    Hpy(*S) = 50;
+    Soc(*S) = 50;
+    Hyg(*S) = 100;
+    Mny(*S) = 0;
+}
+
+void Makan(Stats* S){
+    if (Mny(*S) < 5){
+        printf("Uang tidak cukup\n");
+        return;
+    }
+    Hpy(*S) += 5;
+    if (Hpy(*S) > 100){
+        Hpy(*S) = 100;
+    }
+    Mny(*S) -= 5;
+    KurangHlt(*S, 5);
+}
+
+void Kerja(Stats* S){
+    Hpy(*S) -= 10;
+    if (Hpy(*S) < 0){
+        Hpy(*S) = 0;
+    }
+    Mny(*S) += 30;
+    KurangHlt(*S, 15);
+}
+
+void Nongkrong(Stats* S){
+    if (Mny(*S) < 5) {
+        printf("Uang tidak cukup\n");
+        return;
+    }
+    Hpy(*S) += 3;
+    if (Hpy(*S) > 100){
+        Hpy(*S) = 100;
+    }
+    Soc(*S) += 3;
+    if (Soc(*S) > 100){
+        Soc(*S) = 100;
+    }
+    Mny(*S) -= 5;
+    KurangHlt(*S,5);
+}
+
+void MainGadget(Stats* S){
+    if (Mny(*S) < 5) {
+        printf("Uang tidak cukup\n");
+        return;
+    }
+    Soc(*S) -= 15;
+    if (Soc(*S) < 0){
+        Soc(*S) = 0;
+    }
+    Hpy(*S) += 7;
+    if (Hpy(*S) > 100){
+        Hpy(*S) = 100;
+    }
+    Mny(*S) -= 5;
+    KurangHlt(*S,10);
+}
+
+void Mandi(Stats* S){
+    if (Mny(*S) < 5) {
+        printf("Uang tidak cukup\n");
+        return;
+    }
+    Hyg(*S) += 20;
+    if (Hyg(*S) > 100){
+        Hyg(*S) = 100;
+    }
+    Mny(*S) -= 5;
+    KurangHlt(*S,5);
+}
+
+void Olahraga(Stats* S){
+    if (Mny(*S) < 15){
+        printf("Uang tidak cukup\n");
+        return;
+    }
+    Hyg(*S) -= 25;
+    if (Hyg(*S) < 0){
+        Hyg(*S) = 0;
+    }
+    Hpy(*S) += 15;
+    if (Hpy(*S) > 100){
+        Hpy(*S) = 100;
+    }
+    Mny(*S) -= 15;
+    KurangHlt(*S,10);
+}
+
+void Belanja(Stats* S){
+    if (Mny(*S) < 30){
+        printf("Uang tidak cukup\n");
+        return;
+    }
+    Hpy(*S) += 25;
+    if (Hpy(*S) > 100){
+        Hpy(*S) = 100;
+    }
+    Mny(*S) -= 40;
+    KurangHlt(*S,10);
+}
+
+void MainBG(Stats* S){
+    if (Mny(*S) < 10){
+        printf("Uang tidak cukup\n");
+        return;
+    }
+    Soc(*S) += 10;
+    if (Soc(*S) > 100){
+        Soc(*S) = 100;
+    }
+    Hpy(*S) += 5;
+    if (Hpy(*S) >= 100){
+        Hpy(*S) = 100;
+    }
+    Mny(*S) -= 10;
+    KurangHlt(*S,5);
+}
+
+void Nonton(Stats* S){
+    if (Mny(*S) < 10){
+        printf("Uang tidak cukup\n");
+        return;
+    }
+    Hpy(*S) += 10;
+    if (Hpy(*S) > 100){
+        Hpy(*S) = 100;
+    }
+    Mny(*S) -= 10;
+    KurangHlt(*S, 5);
+}
+
+void Bersih(Stats* S){
+    if (Mny(*S) < 5){
+        printf("Uang tidak cukup\n");
+        return;
+    }
+    Hyg(*S) += 10;
+    if (Hyg(*S) > 100){
+        Hyg(*S) = 100;
+    }
+    Hpy(*S) += 5;
+    if (Hpy(*S) > 100){
+        Hpy(*S) = 100;
+    }
+    Mny(*S) -= 5;
+    KurangHlt(*S, 5);
+}
+
+void KurangHlt(Stats* S, Int W){
+    Hlt(*S) -= W;
+}
--- hilmy.h
+++ hilmy.h
@@ -1,7 +1,62 @@
-#ifndef _Titik_h_
-#define _Titik_h_
+#ifndef _HiLmy_h_
+#define _Hilmy_h_
 
 #include <stdio.h>
 #include <stdlib.h>
+
+//Struct Stats
+typedef struct Stats
+{
+    /* data */
+    int Hlt;
+    int Hpy;
+    int Soc;
+    int Hyg;
+    int Mny;
+};
+
+// Selektor
+#define Hlt(S) (S).Hlt
+#define Hpy(S) (S).Hpy
+#define Soc(S) (S).Soc
+#define Hyg(S) (S).Hyg
+#define Mny(S) (S).Mny
+
+//Konstruktor
+void StartStats(Stats* S)
+//Inisiasi status awal
+
+void Makan(Stats* S)
+//fungsi kegiatan makan
+
+void Kerja(Stats* S)
+//fungsi kegiatan kerja
+
+void Nongkrong(Stats* S)
+//fungsi kegiatan nongkrong
+
+void MainHP(Stats* S)
+//fungsi kegiatan main hp
+
+void Mandi(Stats* S)
+//fungsi kegiatan mandi
+
+void Olahraga(Stats* S)
+//fungsi kegiatan olahraga
+
+void Belanja(Stats* S)
+//fungsi kegiatan belanja
+
+void MainBG(Stats* S)
+//fungsi kegiatan main board game
+
+void Nonton(Stats* S)
+//fungsi kegiatan nonton film
+
+void Bersih(Stats* S)
+//fungsi kegiatan bersih-bersih rumah
+
+void KurangHlt(Stats* S, Int W)
+//fungsi untuk mengurangi health tiap ronde
 
 #endif
